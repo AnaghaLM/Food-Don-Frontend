d0c4570b8f0181f2f3e128f072ad6da0603a6bd1
AnaghaLM <anaghalm4@gmail.com>
2026-01-19 11:50:18 +0530

    changed the import into path

--- src/admin/pages/AdminDonationDetails.jsx
+++ src/admin/pages/AdminDonationDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import AdminSidebar from "../components/AdminSidebar";
+import AdminSidebar from "../../components/AdminSidebar";
 import {
   getAdminDonationDetailsAPI,
   getSingleDonorAdminAPI,
